8d85d4fb74ec36cd3328ebd977c496167d77dfd1
LeftTry <50211739+LeftTry@users.noreply.github.com>
2022-09-27 13:30:38 +0300

    Update views.py

--- mysite/first/views.py
+++ mysite/first/views.py
@@ -13,21 +13,6 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
-def book1(request):
-    return render(request, '1.html')
-
-def book2(request):
-    return render(request, '2.html')
-
-def book3(request):
-    return render(request, '3.html')
-
-def book4(request):
-    return render(request, '4.html')
-
-def book5(request):
-    return render(request, '5.html')
-
-def book6(request):
-    return render(request, '5.html')
-
+def view_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'book.html', {'book': book})
