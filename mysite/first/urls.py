ddab9e0b1092694616ed904b028fae3aae517ce5
LeftTry <50211739+LeftTry@users.noreply.github.com>
2022-09-27 13:31:11 +0300

    Update urls.py

--- mysite/first/urls.py
+++ mysite/first/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path
-
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('book/1/', views.book1, name='book1'),
-    path('book/2/', views.book1, name='book1'),
-    path('book/3/', views.book2, name='book2'),
-    path('book/4/', views.book3, name='book3'),
-    path('book/5/', views.book4, name='book4'),
-    path('book/6/', views.book5, name='book5'),
+    path('', include('first.urls')),
+    path('admin/', admin.site.urls),
 ]
